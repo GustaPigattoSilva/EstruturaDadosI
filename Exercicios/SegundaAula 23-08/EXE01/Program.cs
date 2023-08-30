a92a78639d80c281759af840d1139b7da8611c36
Gustavo Pigatto da Silva <416835@unoesc.lan>
2023-08-30 20:30:59 -0300

    Primeiro Commit

---
+++ Exercicios/SegundaAula 23-08/EXE01/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = {1, 8, -6, 2, 11, 7, 3, -3, 9, 10};
+
+Console.WriteLine("Números Aleatórios");
+for (int i = 0; i <numbers.Length; i++)
+{
+    Console.WriteLine( $"numbers[{i}] = {numbers[i]}" );
+}
+
+Array.Sort(numbers);
+
+Console.WriteLine("Números Ordenados do Menor para o Maior");
+for (int i = 0; i <numbers.Length; i++)
+{
+    Console.WriteLine( $"numbers[{i}] = {numbers[i]}" );
+}
