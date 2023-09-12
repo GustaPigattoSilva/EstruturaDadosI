5509c43cb3677e68ad82a694a53ddd8e621577d9
GustavoPigattoSilva <katsuoiapmes@gmail.com>
2023-09-12 20:24:28 -0300

    PilhaSTR

---
+++ Exercicios/PilhaDeString/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        StackOfString stack = new StackOfString(5);
+
+        stack.Push("Nada");
+        stack.Push("Não Entendi");
+        stack.Push("EU");
+
+        Console.WriteLine("Elemento no topo da pilha: " + stack.Peek());
+
+        Console.WriteLine("Desempilhando elementos:");
+        while (!stack.IsEmpty())
+        {
+            Console.WriteLine(stack.Pop());
+        }
+
+        Console.WriteLine("Número de elementos na pilha: " + stack.Count());
+    }
+}
