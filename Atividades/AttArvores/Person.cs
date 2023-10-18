ae5e7ed1362e3df575ac77d9d2d2c7ab743edd97
GustavoPigattoSilva <katsuoiapmes@gmail.com>
2023-10-17 22:51:48 -0300

    AttLivro

---
+++ Atividades/AttArvores/Person.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public Person () { }
+
+    public Person(int id, string name, string role)
+    {
+        Id = id;
+        Name = name;
+        Role = role;
+    }
+}
